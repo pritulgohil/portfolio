b74cf34ce1325d4b67d320cd615b54b358b417e7
Pritul Gohil <34166907+pritulgohil@users.noreply.github.com>
2025-01-22 19:36:36 -0500

    Fixed styles for link

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -7,8 +7,15 @@ const Footer = () => {
         <div className={styles.footerContent}>
           <h1 className={styles.getInTouch}>Get in Touch</h1>
           <p className={styles.contactText}>
-            Feel free to reach out with a direct question on LinkedIn, and I'll
-            get back to you as soon as possible.
+            Feel free to reach out with a direct question on{" "}
+            <a
+              className={styles.link}
+              href="https://www.linkedin.com/in/pritul-gohil/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+            , and I'll get back to you as soon as possible.
           </p>
         </div>
         <div className={styles.divider}>
@@ -16,7 +23,14 @@ const Footer = () => {
         </div>
         <div className={styles.credits}>
           <span>
-            Portfolio made with NextJs, source code available on Github
+            Portfolio made with NextJs, source code available on{" "}
+            <a
+              className={styles.link}
+              href="https://github.com/pritulgohil/portfolio"
+              target="_blank"
+            >
+              Github
+            </a>
           </span>
         </div>
       </div>
